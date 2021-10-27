2e3b5b0c0425239552cf347b514f730040632fa7
Sebastian Navia <sebasnavia@hotmail.com>
2021-10-27 09:51:24 -0500

    correction

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,50 +1,53 @@
 package ui;
 import java.io.IOException;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import model.FIBA;
 
 
-public class Main extends Application {
+public class Main extends Application{
+	
+	private FIBAGUI FIBAGUI;
+	private FIBA FIBA;
+	
+	public Main() throws ClassNotFoundException, IOException {
+		FIBA = new FIBA();
+		FIBAGUI = new FIBAGUI(FIBA);
+		
+	}
+		public static void main(String[] args) {
+			launch(args);
 
-    private final FIBAGUI FIBAGUI;
-    private FIBA FIBA;
+		}
+		
+		@Override
+	    public void start(Stage primaryStage) throws Exception {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Load.fxml"));
 
-    public Main() throws ClassNotFoundException, IOException {
-        FIBA = new FIBA();
-        FIBAGUI = new FIBAGUI(FIBA);
+			fxmlLoader.setController(FIBAGUI);
+			Parent root = fxmlLoader.load();
+			
+			Scene scene = new Scene(root, 705, 410);
+			
+	        
+	        
+			primaryStage.setScene(scene);
+			
+			
+			primaryStage.setTitle("Federacion Internacional de Baloncesto");
+			
+			primaryStage.show();
+			
+			
 
-    }
-    public static void main(String[] args) {
-        launch(args);
+	    }
 
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Load.fxml"));
-
-        fxmlLoader.setController(FIBAGUI);
-        Parent root = fxmlLoader.load();
-
-        Scene scene = new Scene(root);
-
-
-
-        primaryStage.setScene(scene);
-
-
-        primaryStage.setTitle("Federacion Internacional de Baloncesto");
-
-        primaryStage.show();
-
-    }
-
-
+	
 
 }
