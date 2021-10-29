bd43162ae66ad8384e1d05508f47510b652e4ad6
Dani <danirg1414@outlook.com>
2021-10-28 21:55:59 -0500

    Search BST method modification

--- src/Collections/IBST.java
+++ src/Collections/IBST.java
@@ -1,8 +1,11 @@
 package Collections;
 
+import java.util.ArrayList;
+
 public interface IBST<T,S>{
     public void insert(T node, S key);
-    public BSTNode<T,S> search(S key);
+    public ArrayList<T> search(S key);
+    public BSTNode<T,S> searchOneValue(S key);
     public void delete(BSTNode<T,S> keyToDelete);
     public BSTNode<T,S> getSuccessor(BSTNode<T,S> value);
     public BSTNode<T,S> getMinimum(BSTNode<T,S> value);
--- src/Test/BSTTest.java
+++ src/Test/BSTTest.java
@@ -2,6 +2,8 @@ package Test;
 import Collections.BST;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class BSTTest {
@@ -26,14 +28,19 @@ public class BSTTest {
 
     @Test
     public void searchTest(){
+            BST<String, Integer> bst = setupStage1();
+            bst.insert("a",5);
+            bst.insert("b",7);
+            bst.insert("c",6);
+            bst.insert("d",5);
+            bst.insert("e",4);
+            bst.insert("f",1);
+            bst.insert("g",5);
+            ArrayList<String> a = new ArrayList<>();
+            a.add("a");a.add("d");a.add("g");
+            assertEquals(a,bst.search(5));
 
-        BST<String, Integer> bst = setupStage1();
-        bst.insert("a",3);
-        bst.insert("b",2);
-        bst.insert("c",8);
-        assertEquals("b", bst.search(2).getValue());
-        assertEquals("a", bst.search(3).getValue());
-        assertEquals("c", bst.search(8).getValue());
+
     }
 
     @Test
@@ -47,7 +54,7 @@ public class BSTTest {
         assertEquals("a", bst.getRoot().getValue());
         assertEquals("c", bst.getSuccessor(bst.getRoot()).getValue());
         assertNotEquals("f", bst.getSuccessor(bst.getRoot()).getValue());
-        assertEquals("f", bst.getSuccessor(bst.search(8)).getValue());
+        assertEquals("f", bst.getSuccessor(bst.searchOneValue(8)).getValue());
     }
 
     @Test
@@ -57,8 +64,8 @@ public class BSTTest {
         tree.insert("b",4);
         tree.insert("c",5);
 
-        assertEquals("a",tree.getMinimum(tree.search(3)).getValue());
-        assertNotEquals("c",tree.getMinimum(tree.search(4)).getValue());
+        assertEquals("a",tree.getMinimum(tree.searchOneValue(3)).getValue());
+        assertNotEquals("c",tree.getMinimum(tree.searchOneValue(4)).getValue());
     }
 
     @Test
@@ -68,8 +75,8 @@ public class BSTTest {
         tree.insert("b",4);
         tree.insert("c",5);
 
-        assertEquals("c",tree.getMaximum(tree.search(5)).getValue());
-        assertNotEquals("b",tree.getMaximum(tree.search(3)).getValue());
+        assertEquals("c",tree.getMaximum(tree.searchOneValue(5)).getValue());
+        assertNotEquals("b",tree.getMaximum(tree.searchOneValue(3)).getValue());
     }
 
     @Test
@@ -79,9 +86,9 @@ public class BSTTest {
         tree.insert("b",4);
         tree.insert("c",5);
 
-        assertFalse(tree.isLeaf(tree.search(3)));
-        assertFalse(tree.isLeaf(tree.search(4)));
-        assertTrue(tree.isLeaf(tree.search(5)));
+        assertFalse(tree.isLeaf(tree.searchOneValue(3)));
+        assertFalse(tree.isLeaf(tree.searchOneValue(4)));
+        assertTrue(tree.isLeaf(tree.searchOneValue(5)));
     }
 
     @Test
@@ -115,14 +122,14 @@ public class BSTTest {
         tree.insert("c",5);
         tree.insert("f",3);
 
-        assertEquals(tree.search(4),tree.search(5).getParent());
-        assertEquals(tree.search(4),tree.search(3).getParent());
-        assertNotNull(tree.search(4).getRight());
-        assertNotNull(tree.search(4).getLeft());
-        tree.delete(tree.search(5));
-        assertNull(tree.search(4).getRight());
-        tree.delete(tree.search(3));
-        assertNull(tree.search(4).getLeft());
+        assertEquals(tree.searchOneValue(4),tree.searchOneValue(5).getParent());
+        assertEquals(tree.searchOneValue(4),tree.searchOneValue(3).getParent());
+        assertNotNull(tree.searchOneValue(4).getRight());
+        assertNotNull(tree.searchOneValue(4).getLeft());
+        tree.delete(tree.searchOneValue(5));
+        assertNull(tree.searchOneValue(4).getRight());
+        tree.delete(tree.searchOneValue(3));
+        assertNull(tree.searchOneValue(4).getLeft());
     }
 
     @Test
@@ -135,14 +142,14 @@ public class BSTTest {
         tree.insert("d",2);
         tree.insert("e",4);
         tree.insert("f",1);
-        assertEquals(tree.getRoot(), tree.search(5));
-        assertEquals(tree.search(5),tree.search(7).getParent());
-        assertEquals(tree.search(6), tree.search(7).getLeft());
-        assertNull(tree.search(7).getRight());
-        tree.delete(tree.search(7));
-        assertNull(tree.search(7));
-        assertEquals(tree.search(6).getValue(), tree.search(5).getRight().getValue());
-        assertEquals(tree.search(5),tree.search(6).getParent());
+        assertEquals(tree.getRoot(), tree.searchOneValue(5));
+        assertEquals(tree.searchOneValue(5),tree.searchOneValue(7).getParent());
+        assertEquals(tree.searchOneValue(6), tree.searchOneValue(7).getLeft());
+        assertNull(tree.searchOneValue(7).getRight());
+        tree.delete(tree.searchOneValue(7));
+        assertNull(tree.searchOneValue(7));
+        assertEquals(tree.searchOneValue(6).getValue(), tree.searchOneValue(5).getRight().getValue());
+        assertEquals(tree.searchOneValue(5),tree.searchOneValue(6).getParent());
     }
 
     @Test
@@ -158,37 +165,39 @@ public class BSTTest {
         tree.insert("g",8);
 
         //Deleting key 7, this node has two children
-        tree.delete(tree.search(7));
-        assertNull(tree.search(7));
-        assertNotNull(tree.search(5).getRight());
-        assertEquals(tree.search(8).getValue(), tree.search(5).getRight().getValue());
-        assertNull(tree.search(8).getRight());
-        assertNotNull(tree.search(8).getLeft());
-        assertEquals(tree.search(6).getValue(),tree.search(8).getLeft().getValue());
-        assertEquals(tree.search(5).getValue(),tree.search(8).getParent().getValue());
-        assertEquals(tree.search(8).getValue(),tree.search(6).getParent().getValue());
+        tree.delete(tree.searchOneValue(7));
+        assertNull(tree.searchOneValue(7));
+        assertNotNull(tree.searchOneValue(5).getRight());
+        assertEquals(tree.searchOneValue(8).getValue(), tree.searchOneValue(5).getRight().getValue());
+        assertNull(tree.searchOneValue(8).getRight());
+        assertNotNull(tree.searchOneValue(8).getLeft());
+        assertEquals(tree.searchOneValue(6).getValue(),tree.searchOneValue(8).getLeft().getValue());
+        assertEquals(tree.searchOneValue(5).getValue(),tree.searchOneValue(8).getParent().getValue());
+        assertEquals(tree.searchOneValue(8).getValue(),tree.searchOneValue(6).getParent().getValue());
         //Deleting key 2, this node has two children
-        tree.delete(tree.search(2));
-        assertNull(tree.search(2));
-        assertNotNull(tree.search(5).getLeft());
-        assertEquals(tree.search(4).getValue(), tree.search(5).getLeft().getValue());
-        assertNull(tree.search(4).getRight());
-        assertNotNull(tree.search(4).getLeft());
-        assertEquals(tree.search(1).getValue(),tree.search(4).getLeft().getValue());
-        assertEquals(tree.search(5).getValue(),tree.search(4).getParent().getValue());
-        assertEquals(tree.search(4).getValue(),tree.search(1).getParent().getValue());
+        tree.delete(tree.searchOneValue(2));
+        assertNull(tree.searchOneValue(2));
+        assertNotNull(tree.searchOneValue(5).getLeft());
+        assertEquals(tree.searchOneValue(4).getValue(), tree.searchOneValue(5).getLeft().getValue());
+        assertNull(tree.searchOneValue(4).getRight());
+        assertNotNull(tree.searchOneValue(4).getLeft());
+        assertEquals(tree.searchOneValue(1).getValue(),tree.searchOneValue(4).getLeft().getValue());
+        assertEquals(tree.searchOneValue(5).getValue(),tree.searchOneValue(4).getParent().getValue());
+        assertEquals(tree.searchOneValue(4).getValue(),tree.searchOneValue(1).getParent().getValue());
 
        //Deleting  key 4, this node has one child---> Testing case 2
-        tree.delete(tree.search(4));
-        assertNull(tree.search(2));
-        assertEquals(tree.search(1).getValue(),tree.search(5).getLeft().getValue());
-        assertEquals(tree.search(5).getValue(),tree.search(1).getParent().getValue());
+        tree.delete(tree.searchOneValue(4));
+        assertNull(tree.searchOneValue(2));
+        assertEquals(tree.searchOneValue(1).getValue(),tree.searchOneValue(5).getLeft().getValue());
+        assertEquals(tree.searchOneValue(5).getValue(),tree.searchOneValue(1).getParent().getValue());
 
         //Deleting key 1, this node has not child
-        tree.delete(tree.search(1));
-        assertNull(tree.search(1));
-        assertNull(tree.search(5).getLeft());
+        tree.delete(tree.searchOneValue(1));
+        assertNull(tree.searchOneValue(1));
+        assertNull(tree.searchOneValue(5).getLeft());
     }
+
+
 
 
 }
