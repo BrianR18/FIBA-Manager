513e3db3d5b4de249b6988b2006ea7111d2a8ae6
brianR <briomero76@gmail.com>
2021-11-01 01:00:21 -0500

    saad

---
+++ src/ui/FIBAGUIEmergent.java
@@ -0,0 +1,14 @@
+package ui;
+import javafx.fxml.FXML;
+import model.*;
+
+public class FIBAGUIEmergent {
+    private FIBA fiba;
+
+    public FIBAGUIEmergent(FIBA f){
+        fiba = f;
+    }//End constructor
+
+    @FXML
+    public void showPlayerInformationWindow()
+}
